9a87c1faa4047c540541e011d6c5348e38bb6e2f
stormeuh <elias.storme@gmail.com>
2017-05-20 16:45:29 +0200

    Implemented id generation using primeiterator

--- src/rpg/Armor.java
+++ src/rpg/Armor.java
@@ -1,5 +1,19 @@
 package rpg;
 
+import rpg.utility.PrimeIterator;
+
 public class Armor extends Item {
+	
+	private static PrimeIterator idGenerator = new PrimeIterator();
+	private final long ID;
+	
+	public Armor(){
+		ID = generateID();
+	}
+	
+	@Override
+	protected long generateID() {
+		return idGenerator.nextLong();
+	}
 
 }
