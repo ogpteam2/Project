d8a760824e984e4e0da8d0e59fba9220f3df4382
stormeuh <elias.storme@gmail.com>
2017-05-20 22:40:57 +0200

    shite

--- src/rpg/Armor.java
+++ src/rpg/Armor.java
@@ -11,9 +11,21 @@ public class Armor extends Item {
 		ID = generateID();
 	}
 	
+	/**
+	 * Generates an ID for the piece of armor
+	 * @return ID for the armor
+	 */
 	@Override
 	protected long generateID() {
 		return idGenerator.nextLong();
+	}
+	
+	/**
+	 * Returns the ID of this piece of armor.
+	 * @return armor ID
+	 */
+	public long getID(){
+		return this.ID;
 	}
 
 }
