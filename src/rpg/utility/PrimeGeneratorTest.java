9953f434fc62781c3103e27970b67a22ff9dd233
Elias Storme <elias.storme@gmail.com>
2017-05-23 14:57:35 +0200

    Renamed tests

---
+++ src/rpg/utility/PrimeGeneratorTest.java
@@ -0,0 +1,27 @@
+package rpg.utility;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrimeGeneratorTest {
+	
+	PrimeGenerator it;
+	long[] littlePrimeList = {2,3,5,7,11,13,17,19};
+	long[] generatedPrimeList;
+	
+	@Before
+	public void setUp() {
+		it = new PrimeGenerator();
+		generatedPrimeList = new long[littlePrimeList.length];
+	}
+
+	@Test
+	public void firstElementsCheck() {
+		for(int i = 0; i < littlePrimeList.length; i++){
+			generatedPrimeList[i] = it.nextID();
+		}
+		assertArrayEquals(littlePrimeList,generatedPrimeList);
+	}
+}
