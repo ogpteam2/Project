0afd18a2654b65e780841d2dd5895b2be995c5f9
stormeuh <elias.storme@gmail.com>
2017-05-25 21:48:18 +0200

    Advanced work on valueclass

--- src/rpg/inventory/DucatAmount.java
+++ src/rpg/inventory/DucatAmount.java
@@ -5,10 +5,22 @@ import be.kuleuven.cs.som.annotate.Value;
 @Value
 public class DucatAmount implements Comparable<DucatAmount> {
 
+	private Container container = null;
+	
+	public DucatAmount(double amount){
+		this.amount = amount;
+	}
+	
+	private final double amount;
+	
 	@Override
 	public int compareTo(DucatAmount o) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public DucatAmount add(DucatAmount other){
+		
 	}
 
 }
