73c9f89c5c460bc1dd4c55fec36c47264395c64a
stormeuh <elias.storme@gmail.com>
2017-05-25 21:44:38 +0200

    Changed names again

---
+++ src/rpg/inventory/Gear.java
@@ -0,0 +1,9 @@
+package rpg.inventory;
+
+public abstract class Item extends Immobile{
+	
+	public Item(float weight, double value){
+		super(weight);
+	}
+	
+}
--- src/rpg/inventory/Item.java
+++ src/rpg/inventory/Item.java
@@ -1,9 +1,63 @@
 package rpg.inventory;
 
-public abstract class Item extends Immobile{
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+import be.kuleuven.cs.som.annotate.Raw;
+import rpg.utility.IDGenerator;
+
+abstract public class Immobile {
 	
-	public Item(float weight, double value){
-		super(weight);
+	public Immobile(double weight){
+		ID = generateID();
+		if(isValidWeight(weight)){
+			this.weight = weight;
+		} else {
+			this.weight = 0;
+		}
 	}
+	
+	/************************************************
+	 * Identifier
+	 ************************************************/
+	
+	private final long ID;
+	
+	/**
+	 * Generates an ID in accordance with the item type's ID spec.
+	 */
+	public abstract long generateID();
+	
+	/**
+	 * Returns the item ID.
+	 * @return
+	 */
+	@Raw
+	@Basic
+	public long getID(){
+		return this.ID;
+	}
+	
+	/************************************************
+	 * Value
+	 ************************************************/
+	
+	private int value;
+	
+	/************************************************
+	 * Weight
+	 ************************************************/
+	
+	private final double weight;
+	
+	@Raw
+	@Immutable
+	public double getWeight(){
+		return this.weight;
+	}
+	
+	public boolean isValidWeight(double weight){
+		return weight >= 0;
+	}
+	
 	
 }
