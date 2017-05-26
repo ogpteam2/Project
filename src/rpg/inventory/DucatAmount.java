6704337f0279f8e22907eb3ca8bd7625b7c1fdf4
Elias Storme <elias.storme@gmail.com>
2017-05-26 23:17:15 +0200

    Implemented arithmetical and logical methods

--- src/rpg/inventory/DucatAmount.java
+++ src/rpg/inventory/DucatAmount.java
@@ -1,26 +1,112 @@
 package rpg.inventory;
 
+import java.math.BigDecimal;
+
 import be.kuleuven.cs.som.annotate.Value;
 
 @Value
 public class DucatAmount implements Comparable<DucatAmount> {
 
-	private Container container = null;
-	
-	public DucatAmount(double amount){
-		this.amount = amount;
+	public DucatAmount(BigDecimal amount) throws IllegalArgumentException {
+		if (amount == null)
+			throw new IllegalArgumentException();
+		else
+			this.amount = amount;
 	}
-	
-	private final double amount;
-	
+
+	public DucatAmount() {
+		this(BigDecimal.ONE);
+	}
+
+	private final BigDecimal amount;
+
+	/******************************************
+	 * Logic
+	 ******************************************/
+
 	@Override
-	public int compareTo(DucatAmount o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(DucatAmount other) throws ClassCastException {
+		if (other == null) {
+			throw new ClassCastException();
+		} else {
+			return this.getValue().compareTo(other.getValue());
+		}
 	}
 	
-	public DucatAmount add(DucatAmount other){
-		
+	public boolean equals(DucatAmount other) throws ClassCastException {
+		return this.compareTo(other) == 0;
+	}
+	
+	public boolean isGreaterThan(DucatAmount other) throws ClassCastException{
+		return this.compareTo(other) == 1;
+	}
+	public String toString() {
+		return (this.getValue().toString() + "ducats");
+	}
+
+	public BigDecimal getValue() {
+		return this.amount;
+	}
+
+	/******************************************
+	 * Arithmetic
+	 ******************************************/
+
+	/**
+	 * Adds the values of two DucatAmounts together and returns a new DucatAmount with that value.
+	 * @param other
+	 * 		  Second DucatAmount of which the value is to be added
+	 * @return new DucatAmount with the sum of the value of the prime object and other as value
+	 * 		|  return.getValue() == this.getValue() + other.getValue()
+	 */
+
+	public DucatAmount add(DucatAmount other) {
+		BigDecimal total = BigDecimal.ZERO;
+		total.add(this.getValue());
+		total.add(other.getValue());
+		return new DucatAmount(total);
+	}
+
+	public DucatAmount subtract(DucatAmount other) throws ArithmeticException {
+		BigDecimal total = BigDecimal.ZERO;
+		total.add(this.getValue());
+		total.subtract(other.getValue());
+		if (total.signum() == -1)
+			throw new ArithmeticException("Cannot have a negative amount of ducats!");
+		else
+			return new DucatAmount(total);
+	}
+	
+	/**
+	 * Multiplies the value of this ducatamount and returns a new ducatamount with the multiplied value.
+	 * @param factor
+	 * 		  Factor to be multiplied by
+	 * @return DucatAmount with the multiplied value
+	 * 		|  return.getValue() == this.getValue() * factor
+	 */
+	public DucatAmount multiply(BigDecimal factor){
+		BigDecimal result = this.getValue();
+		result.multiply(factor);
+		return new DucatAmount(result);
+	}
+	
+	
+	/**
+	 * Overloaded method accepting a double as multiplication factor
+	 * @see multiply(BigDecimal factor)
+	 */
+	public DucatAmount multiply(double factor){
+		BigDecimal bigFactor = new BigDecimal(factor);
+		return multiply(bigFactor);
+	}
+	
+	/**
+	 * Overloaded method accepting an integer as multiplication factor
+	 * @see multiply(BigDecimal factor)
+	 */
+	public DucatAmount multiply(int factor){
+		BigDecimal bigFactor = new BigDecimal(factor);
+		return multiply(bigFactor);
 	}
 
 }
