541eda6ac82f87025127755ea6bb685149e0f0a6
Elias Storme <elias.storme@gmail.com>
2017-05-27 15:36:13 +0200

    Added anchorpoint variables

--- src/rpg/Hero.java
+++ src/rpg/Hero.java
@@ -2,8 +2,16 @@ package rpg;
 
 import java.util.regex.Pattern;
 
+import rpg.inventory.Anchorpoint;
+
 public class Hero extends Mobile {
 
+	private Anchorpoint body;
+	private Anchorpoint leftHand;
+	private Anchorpoint rightHand;
+	private Anchorpoint belt;
+	private Anchorpoint back;
+	
 	public Hero(String name) throws IllegalArgumentException {
 		super(name);
 		this.
