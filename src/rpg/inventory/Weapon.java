a53a3cfe9b2f722f85be3532ab5db1ae25ab64bf
stormeuh <elias.storme@gmail.com>
2017-05-28 19:49:11 +0200

    Implemented value

--- src/rpg/inventory/Weapon.java
+++ src/rpg/inventory/Weapon.java
@@ -6,46 +6,47 @@ import rpg.value.DucatAmount;
 import rpg.value.Weight;
 
 public class Weapon extends Item {
-	
+
 	private static WeaponIDGenerator idGenerator = new WeaponIDGenerator();
-	
+
 	private final static int MAX_DAMAGE = 100;
-	
+	private final static DucatAmount VALUE_PER_DAMAGE = new DucatAmount(2);
+
 	public Weapon(Weight weight, int damage) {
 		super(weight);
 		setDamage(damage);
 	}
 
-	protected IDGenerator getIDGenerator(){
+	protected IDGenerator getIDGenerator() {
 		return idGenerator;
 	}
-	
+
 	private int damage = 0;
-	
-	public int getDamage(){
+
+	public int getDamage() {
 		return this.damage;
 	}
-	
-	public void setDamage(int damage){
+
+	public void setDamage(int damage) {
 		assert canHaveAsDamage(damage);
 		this.damage = damage;
 	}
-	
-	public boolean canHaveAsDamage(int damage){
-		boolean divisibleBySeven = (damage%7==0);
+
+	public boolean canHaveAsDamage(int damage) {
+		boolean divisibleBySeven = (damage % 7 == 0);
 		boolean withinRange = (damage > 0 && damage <= MAX_DAMAGE);
 		return divisibleBySeven && withinRange;
 	}
 
 	@Override
 	protected boolean canHaveAsValue(DucatAmount value) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean lowerRange = value.compareTo(new DucatAmount(1)) == 1;
+		boolean upperRange = value.compareTo(new DucatAmount(200)) != 1;
+		return lowerRange && upperRange;
 	}
 
 	@Override
 	public DucatAmount getValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return VALUE_PER_DAMAGE.multiply(getDamage());
 	}
 }
